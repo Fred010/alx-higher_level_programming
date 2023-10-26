99fa27a9222d2d82b5015fd7dea81b56333d8323
Fred010 <fjbaafi18@gmail.com>
2023-10-25 19:18:53 -0700

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
+"""Define a class Square"""
+
 
 class Square:
+    """where the class Square is built"""
 
     def __init__(self, size=0):
+        """initialize new Square
+
+        Args:
+            size(int): size of new square"""
         self.__size = size
 
     @property
     def size(self):
+        """Get or Set current size of Square"""
         return self.__size
 
     @size.setter
@@ -19,4 +27,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return current area of Square"""
         return self.__size ** 2

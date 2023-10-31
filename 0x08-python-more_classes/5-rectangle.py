e8609af3590df1534f4c94cf265e1a6eaa2c12a2
Fred010 <fjbaafi18@gmail.com>
2023-10-30 20:02:25 -0700

    Files and submissions

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """shows a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """initialize a new Rectangle.
+
+        Args:
+            width (int): width of the new rectangle.
+            height (int): height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get or Set the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get or Set the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the area of the Rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """returns the  of the Rectangle.
+
+        shows the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rectangle = []
+        for i in range(self.__height):
+            [rectangle.append("#") for j in range(self.__width)]
+            if i != self.__height - 1:
+                rectangle.append("\n")
+        return "".join(rectangle)
+
+    def __repr__(self):
+        """returns the string representation of the Rectangle."""
+        rectangle = "Rectangle(" + str(self.__width)
+        rectangle += ", " + str(self.__height) + ")"
+        return rectangle
+
+    def __del__(self):
+        """shows a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,12 @@
+# Python - More Classes and Objects
+
+classes are used to define the blueprint for creating objects
+
+objects are instances of classes and with attributes and methods
+
+* variables -> attributes
+* methods -> functions
+
+In this project, we will consider:
+* inheritance
+* encapsulation

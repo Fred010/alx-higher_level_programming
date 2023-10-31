367d9eec055298cf3fabf138df4be988d6fd4cf3
Fred010 <fjbaafi18@gmail.com>
2023-10-30 20:27:19 -0700

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
         number_of_instances (int): The number of Rectangle instances.
     """
 
-    num_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """initialize a new Rectangle.
@@ -18,7 +18,7 @@ class Rectangle:
             width (int): width of the new rectangle.
             height (int): height of the new rectangle.
         """
-        type(self).num_of_instances += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -81,5 +81,5 @@ class Rectangle:
 
     def __del__(self):
         """outputs a message for every deletion of a Rectangle."""
-        type(self).num_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")

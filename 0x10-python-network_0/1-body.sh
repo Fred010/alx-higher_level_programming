ce6313d62480e564d0dc1d09d88c8db5843f78a8
Fred010 <fjbaafi18@gmail.com>
2024-01-25 18:35:41 -0800

    curl end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to URL and display response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" $1 | grep -q 200 && curl -s $1
+curl -s -o /dev/stdout -w "%{http_code}" "$1"

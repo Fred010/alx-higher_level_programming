3c45bf952fd9a1871ba2b25a511b2f1ba3268f97
Fred010 <fjbaafi18@gmail.com>
2024-01-25 20:25:39 -0800

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request to 0.0.0.0:5000/catch_me to respond with "You got me!"
-curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -LsX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"

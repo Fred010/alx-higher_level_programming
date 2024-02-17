b367353a4e61bc9978176627521d71d369813530
Fred010 <fjbaafi18@gmail.com>
2024-02-17 07:25:11 -0800

    stats  q

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,19 @@
+"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
+
 import urllib
 
-url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.urlopen(url) as response:
-    body = response.read()
-    print("- Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+def fetch_status():
+    """
+    Fetches the status from https://alx-intranet.hbtn.io/status using urllib.
+
+    Prints the body of the response with tabulation before each line.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.urlopen(url) as response:
+        body = response.read()
+        print("\n".join(["\t- " + line for line in body.splitlines()]))
+
+
+if __name__ == "__main__":
+    fetch_status()

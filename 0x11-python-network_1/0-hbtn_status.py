9915611afe46c0c0452db7bd93c936a6f686b8eb
Fred010 <fjbaafi18@gmail.com>
2024-02-17 07:00:22 -0800

    status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read().decode('utf-8')
+
+print("- Body response:")
+print("\t- type:", type(html))
+print("\t- content:", html)

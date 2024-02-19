8e21effcdda43723c37ed2ab8adcdbf42cd8260c
Fred010 <fjbaafi18@gmail.com>
2024-02-19 05:33:56 -0800

    my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+"""
+Checks if the script is being run directly (not imported as a module)
+scripts to fetch the content of a URL
+"""
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    page = response.read()
-    utf8_content = page.decode('utf-8')
-    print("Body response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(utf8_content))
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+        utf8_content = page.decode('utf-8')
+
+        print("Body response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(utf8_content))

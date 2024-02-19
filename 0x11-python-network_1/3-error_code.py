fb9993d14c5433f083be62d638e9e85317b35112
Fred010 <fjbaafi18@gmail.com>
2024-02-19 14:43:20 -0800

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+"""
+scsript that takes and sends URL and displays response
+"""
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    page_req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(page_req) as response:
+            page = response.read().decode("ascii")
+            print(page)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))

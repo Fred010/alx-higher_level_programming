5b2f8b7cc88a66d87522bcd3d05ee68409484447
Fred010 <fjbaafi18@gmail.com>
2024-02-19 07:01:01 -0800

    status quo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+
 import urllib.request
 
 
 if __name__ == "__main__":
-
     url = "https://alx-intranet.hbtn.io/status"
-
     with urllib.request.urlopen(url) as response:
         page = response.read()
+        utf8_content = page.decode('utf-8')
 
         print("Body response:")
         print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
-        print("\t- utf8 content: {}".format(page.decode("utf-8").strip()))
+        print("\t- utf8 content: {}".format(utf8_content))

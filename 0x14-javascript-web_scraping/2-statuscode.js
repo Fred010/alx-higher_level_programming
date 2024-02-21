aded3a92ba90af045a04eeb0b1f58b85dd00250f
Fred010 <fjbaafi18@gmail.com>
2024-02-20 22:11:24 -0800

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-const https = require('https');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./2-statuscode.js <URL>');
-  process.exit(1);
-}
+const request = require('request');
 
 const url = process.argv[2];
 
-https.get(url, (res) => {
-  console.log(`code: ${res.statusCode}`);
-}).on('error', (err) => {
-  console.error(err);
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('code:', response.statusCode);
 });

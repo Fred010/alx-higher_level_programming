bb337bb786e3857eab4590e56a2f4a223b7ea4a6
Fred010 <fjbaafi18@gmail.com>
2024-02-21 04:23:35 -0800

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,32 +1,4 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const https = require('https');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: ./5-request_store.js <URL> <file_path>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-https.get(url, (res) => {
-  let body = '';
-  res.setEncoding('utf-8');
-  res.on('data', (data) => {
-    body += data;
-  });
-  res.on('end', () => {
-    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-      if (err) {
-        console.error('Error writing file:', err);
-        process.exit(1);
-      }
-      console.log(`Successfully saved the contents of ${url} to ${filePath}`);
-    });
-  });
-}).on('error', (err) => {
-  console.error('Error fetching URL:', err);
-  process.exit(1);
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
